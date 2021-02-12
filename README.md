fefe84d9805133f5703b22c13f8634d74b2ce272
Leonel Abrante <abranteleo@gmail.com>
2021-02-12 10:57:43 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+A summernote plunker example with angularjs directive
---
+++ script.js
@@ -0,0 +1,22 @@
+app = angular.module("app", ["summernote"]);
+app.controller("summerNoteController", function($scope) {
+  $scope.options = {
+    height: 300,
+    focus: true,
+    toolbar: [
+            ['edit',['undo','redo']],
+            ['headline', ['style']],
+            ['style', ['bold', 'italic', 'underline', 'superscript', 'subscript', 'strikethrough', 'clear']],
+            ['fontface', ['fontname']],
+            ['textsize', ['fontsize']],
+            ['fontclr', ['color']],
+            ['alignment', ['ul', 'ol', 'paragraph', 'lineheight']],
+            ['height', ['height']],
+            ['table', ['table']],
+            ['insert', ['link','picture','video','hr']],
+            ['view', ['fullscreen', 'codeview']],
+            ['help', ['help']]
+        ]
+  };
+  $scope.text = "hola";
+});
